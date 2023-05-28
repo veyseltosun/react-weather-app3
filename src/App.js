8f4981e9b1610d325d812d3542e304ea12b01f0b
veyseltosun <postalarim1923@gmail.com>
2023-05-28 21:17:49 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 
-import './App.css';
-
 function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=06dddaec5b6cc50a3ddf0212b8adb297
+  `
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Istanbul</p>
+          </div>
+          <div className="temp">
+           <h1>68</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+               <p>65 F </p>
+
+            </div>
+            <div className="humidity">
+              <p>20%</p>
+            </div>
+            <div className="wind">
+              12 MPH
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
